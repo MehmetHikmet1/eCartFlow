6510ab231b336f423fd365dc7ae9c7399800e7ed
Mehmet Hikmet Demirci <hikmet.demirci@alo-tech.com>
2024-10-29 07:49:11 +0300

    cart flow endpoint created

--- eCartFlow/api/serializers.py
+++ eCartFlow/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from shop.models import ElectronicDevices, FashionProducts, FurnitureProduct
+from shop.models import ElectronicDevices, FashionProducts, FurnitureProduct, Cart, CartItem
 
 class ProductBaseSerializer(serializers.ModelSerializer):
     class Meta:
@@ -19,3 +19,18 @@ class FurnitureProductSerializer(ProductBaseSerializer):
     class Meta(ProductBaseSerializer.Meta):
         model = FurnitureProduct
         fields = ProductBaseSerializer.Meta.fields + ['material', 'dimensions']
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product_type = serializers.ChoiceField(choices=[('electronic', 'Electronic'), ('fashion', 'Fashion'), ('furniture', 'Furniture')])
+    
+    class Meta:
+        model = CartItem
+        fields = ['id', 'cart', 'product_type', 'product_id', 'quantity']
+
+class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Cart
+        fields = ['id', 'user', 'items']
+
--- eCartFlow/api/urls.py
+++ eCartFlow/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ElectronicDeviceViewSet, FashionProductViewSet, FurnitureProductViewSet
+from .views import CartView, ElectronicDeviceViewSet, FashionProductViewSet, FurnitureProductViewSet, ProductFilterView
 
 router = DefaultRouter()
 router.register(r'electronic-devices', ElectronicDeviceViewSet)
@@ -9,4 +9,6 @@ router.register(r'furniture-products', FurnitureProductViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('cart/', CartView.as_view(), name='cart'),
+    path('products/filter/', ProductFilterView.as_view(), name='product-filter'), 
 ]
